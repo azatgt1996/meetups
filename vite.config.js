0bd211c1e00a94086ae7bc175ea823b9630e4877
azatgt1996 <azatgt1996@yandex.ru>
2024-01-18 20:41:29 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue(), splitVendorChunkPlugin()],
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://course-vue.javascript.ru',
+        changeOrigin: true,
+      },
+    },
+  },
+});
